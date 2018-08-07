6c344c4a6169fbab10ebc90a1154c00d5b4e7b4f
romanossipov <39733158+romanossipov@users.noreply.github.com>
2018-08-07 12:03:46 +0200

    Add files via upload

---
+++ api/config/database.php
@@ -0,0 +1,26 @@
+<?php
+class Database{
+ 
+    // specify your own database credentials
+    private $host = "aziedb1rez463.mysql.database.azure.com";
+    private $db_name = "api_db";
+    private $username = "pocwdgaccount";
+    private $password = "YR4+_9gX\[";
+    public $conn;
+ 
+    // get the database connection
+    public function getConnection(){
+ 
+        $this->conn = null;
+ 
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+ 
+        return $this->conn;
+    }
+}
+?>
